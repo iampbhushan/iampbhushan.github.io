d30c8c705f07875f876efea9ce684c48ffd56cc6
Pushpendu Bhushan <34141514+itsmebhushan@users.noreply.github.com>
2018-03-17 16:47:29 +0530

    Create bhushan.js

---
+++ bhushan.js
@@ -0,0 +1,26 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
+
+
+
+
+// Smooth scroll
+ $(document).ready(function() {
+    $("a").on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+            scrollTop: $(hash).offset().top - 120
+        }, 800, function() {
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
